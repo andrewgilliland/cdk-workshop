f4acf3f215dfb12378309630793c10709a4b1471
Andrew Gilliland <andrewpgilliland@gmail.com>
2023-12-23 08:16:30 -0600

    feat: deploys apigateway

--- lambda/hello.ts
+++ lambda/hello.ts
@@ -7,12 +7,10 @@ export const handler: Handler = async (
   try {
     console.log("request:", JSON.stringify(event, undefined, 2));
 
-    const { httpMethod, path, body } = event;
-
     const response = {
       statusCode: 200,
       header: { "Content-Type": "text/plain" },
-      body: JSON.stringify(`Hello from CDK! You've hit ${path}\n`),
+      body: JSON.stringify(`Yo CDK! You've hit ${event.path}\n`),
     };
 
     return response;
